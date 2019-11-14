81525d9278b56428aabc4bf3198185693093334a
Manuel Lang <manuel.lang@fntsoftware.com>
2019-11-14 09:07:51 +0100

    chore: switch api

--- projects/angular2-grid/src/lib/angular2-grid.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Angular2GridDirective } from './angular2-grid.directive';
-
-describe('Angular2GridDirective', () => {
-  it('should create an instance', () => {
-    const directive = new Angular2GridDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- projects/angular2-grid/src/public-api.ts
+++ projects/angular2-grid/src/public-api.ts
@@ -1,8 +1,5 @@
-/*
- * Public API Surface of angular2-grid
- */
-
-export * from './lib/angular2-grid.service';
-export * from './lib/angular2-grid.component';
-export * from './lib/angular2-grid.directive';
-export * from './lib/angular2-grid.module';
+export { NgGrid } from './lib/directives/NgGrid';
+export { NgGridItem } from './lib/directives/NgGridItem';
+export { NgGridPlaceholder } from './lib/components/NgGridPlaceholder';
+export { NgGridConfig, NgGridItemConfig, NgGridItemEvent, NgGridItemSize, NgGridItemPosition, NgGridRawPosition, NgGridItemDimensions } from './lib/interfaces/INgGrid';
+export { NgGridModule } from './lib/NgGrid.module';
